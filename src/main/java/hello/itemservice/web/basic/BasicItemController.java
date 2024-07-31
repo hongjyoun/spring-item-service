b8233c198afed56d7b077f490ff0056b2bf95d01
hongjyoun <hongjyoun@nate.com>
2024-07-31 17:03:25 +0900

    Post Redirect Get

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -65,10 +65,22 @@ public class BasicItemController {
     return "basic/item";
   }
 
-  @PostMapping("/add")
+//  @PostMapping("/add")
   public String addItemV3(@ModelAttribute Item item, Model model) { // 괄호안에 ("item")이 생략되면, 클래스명(Item)의 첫글자만 소문자로 바꾼 item으로 자동으로 만들어준다.
     itemRepository.save(item);
     return "basic/item";
+  }
+
+//  @PostMapping("/add")
+  public String addItemV4(Item item) {
+    itemRepository.save(item);
+    return "basic/item";
+  }
+
+  @PostMapping("/add")
+  public String addItemV5(Item item) { // 괄호안에 ("item")이 생략되면, 클래스명(Item)의 첫글자만 소문자로 바꾼 item으로 자동으로 만들어준다.
+    itemRepository.save(item);
+    return "redirect:/basic/items/" + item.getId();
   }
 
   @GetMapping("/{itemId}/edit")
